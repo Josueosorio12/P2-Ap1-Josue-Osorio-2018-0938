b5a6e8bd51034150585b8a0be5f85a0e53c20ed1
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-11-08 17:58:38 -0400

    Creando el contexto

---
+++ P2-Ap1-Josue-Osorio-2018-0938/DAL/Contexto.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_Ap1_Josue_Osorio_2018_0938.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_Ap1_Josue_Osorio_2018_0938.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<TipoTarea> TipoDeTarea { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"DATA Source = DATA/CitiesControl.db");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TipoTarea>().HasData(new TipoTarea
+            {
+
+                Tipoid = 1,
+                TipodeTarea = "Analisis",
+                Tiempo = 0
+            });
+
+            modelBuilder.Entity<TipoTarea>().HasData(new TipoTarea
+            {
+                Tipoid = 2,
+                TipodeTarea = "Diseno",
+                Tiempo = 0
+            });
+
+            modelBuilder.Entity<TipoTarea>().HasData(new TipoTarea
+            {
+                Tipoid = 3,
+                TipodeTarea = "Programacion",
+                Tiempo = 0
+            });
+
+            modelBuilder.Entity<TipoTarea>().HasData(new TipoTarea
+            {
+                Tipoid = 4,
+                TipodeTarea = "Prueba",
+                Tiempo = 0
+            });
+        }
+    }
+}
