da62c47b7a428ec8584d4af9001e6aa37e6b00af
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-11-08 17:44:28 -0400

    Crenado la entidad TipoTarea

---
+++ P2-Ap1-Josue-Osorio-2018-0938/Entidades/TipoTarea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_Ap1_Josue_Osorio_2018_0938.Entidades
+{
+  public  class TipoTarea
+    {
+        [Key]
+
+
+        public int Tipoid { get; set; }
+        public string TipodeTarea { get; set; }
+        public string Requerimiento { get; set; }
+        public int Tiempo { get; set; }
+    }
+}
+
