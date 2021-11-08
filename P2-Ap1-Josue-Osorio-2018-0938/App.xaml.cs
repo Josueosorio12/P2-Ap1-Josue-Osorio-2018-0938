37919fe193bb5b7abd09c235815572681d839fbc
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-11-08 17:13:37 -0400

    Add project files.

---
+++ P2-Ap1-Josue-Osorio-2018-0938/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P2_Ap1_Josue_Osorio_2018_0938
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
